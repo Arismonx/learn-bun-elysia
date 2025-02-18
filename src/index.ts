cf2b10e76c4007d18369aca865a31ea82d73f0a6
Arismonx <arismonx@gmail.com>
2025-02-18 23:25:25 +0700

    api auth & read-write file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import { Elysia } from "elysia";
+import { Elysia, error } from "elysia";
+import { jwt } from '@elysiajs/jwt'
+import { staticPlugin } from '@elysiajs/static'
+import fs from 'fs/promises';
 
 const app = new Elysia()
+  .use(staticPlugin({
+    assets: './uploads',
+    prefix: '/uploads',
+  }))
+  .use(
+    jwt({
+      name: 'jwt',
+      secret: 'mamafafa'
+    })
+  )
   .get("/", () => "Hello Elysia Framwork")
   .get('/hello', () => {
     return { message: 'hello world' }
@@ -70,6 +83,66 @@ const app = new Elysia()
     }
   }) => {
     return { message: `Delete customer ${params.id}` }
+  })
+
+  .post('/user/signin', async ({ jwt, set, body }: {
+    jwt: any,
+    set: any,
+    body: {
+      username: string
+      password: string
+    }
+  }) => {
+    try {
+      const { username, password } = body;
+      if (username !== "admin" || password !== "1234") {
+        set.status = 401;
+        return { error: "Invalid username or password" }
+      }
+      const token = await jwt.sign({ username }, {
+        expiresIn: '1d'
+      })
+      return { token }
+    } catch {
+      set.status = 500
+      return { error: error }
+    }
+  })
+
+  .get('/user/profile', async ({ jwt, cookie: { auth } }: {
+    jwt: any,
+    cookie: any
+  }) => {
+    const { username } = await jwt.verify(auth.value);
+    return { message: `Hello ${username}` }
+  })
+
+  .get('/user/profileFromToken', async ({ jwt, request }: {
+    jwt: any,
+    request: any
+  }) => {
+    const authorzation = request.headers.get('Authorzation');
+    const token = authorzation?.split(' ')[1];
+    const { username } = await jwt.verify(token);
+    return { message: `Hello ${username}` }
+  })
+
+  .post('/upload-file', ({ body }: { body: { file: File } }) => {
+    const file = body.file;
+    Bun.write('uploads/' + file.name, file);
+    return { message: "File uploaded" }
+  })
+
+  .get('/write-file', () => {
+    Bun.write('uploads/test.txt', 'Hello world');
+    return { message: "File written" }
+  })
+
+  .get('/read-file', async () => {
+    const path = "./uploads/test.txt";
+    const content = Bun.file(path);
+    const text = await content.text();
+    return { message: text }
   })
 
   .listen(3000);
